96096f5b9805a18068ffa643713a6c7b47f67827
ryan susman <ryansusman@gmail.com>
2020-10-11 19:07:39 -0500

    packager

---
+++ connectx_rl/bots/bot_v1/package/submission_template.py
@@ -0,0 +1,17 @@
+def my_agent(observation, configuration):
+    from random import choice
+    import numpy as np
+    this_coice = 0
+    _board_width = 7
+    _board_height = 6
+
+    master_truth_table = {REPLACEME}
+
+    
+    obs = np.reshape(observation.board, (_board_width, _board_height)).T
+    
+    if str(obs) in master_truth_table:
+        this_choice = master_truth_table[str(obs)]
+    else:
+        this_choice = choice([c for c in range(_board_width) if observation.board[c] == 0])
+    return this_choice
---
+++ connectx_rl/bots/bot_v1/packager.py
@@ -0,0 +1,37 @@
+import os
+import json
+import socket
+
+
+# loading configuration...
+print('loading configuration...')
+_config = {}
+with open('config.json') as f:
+    _config = json.load(f)
+
+host_name = socket.gethostname()
+base_directory_key = 'base_dir'
+target = f'{host_name}-base_dir'
+if target in _config['files']['policy']:
+    base_directory_key = target
+
+_master_truth_dir = os.path.join(_config['files']['policy'][base_directory_key],
+                                      _config['files']['policy']['master_truth']['dir'])
+
+_master_truth_file = os.path.join(_config['files']['policy'][base_directory_key],
+                                      _config['files']['policy']['master_truth']['dir'],
+                                      _config['files']['policy']['master_truth']['name'])
+
+f = open(_master_truth_file, "r")
+payload = f.read()
+f.close()
+
+f = open('package//submission_template.py', "r")
+sub_template = f.read()
+f.close()
+
+f = open('package//submission.py', "w")
+f.write(sub_template.replace("{REPLACEME}", payload))
+f.close()
+
+
