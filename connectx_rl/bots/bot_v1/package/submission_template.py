c01ea9ac4df926002fc283ebee4221c46f3f031a
Ryan Susman <ryansusman@gmail.com>
2020-10-14 07:07:27 -0500

    checkpoint

--- connectx_rl/bots/bot_v1/package/submission_template.py
+++ connectx_rl/bots/bot_v1/package/submission_template.py
@@ -15,23 +15,3 @@ def my_agent(observation, configuration):
     else:
         this_choice = choice([c for c in range(_board_width) if observation.board[c] == 0])
     return this_choice
-
-
-from kaggle_environments import evaluate, make, utils
-
-env = make("connectx", debug=True)
-
-win = 0
-loss = 0
-
-for i in range(100):
-    env.reset()
-    # Play as the first agent against default "random" agent.
-    env.run([my_agent, "random"])
-    if env.state[0].reward == 1:
-        win += 1
-    else:
-        loss += 1
-    # env.render(mode="ipython", width=500, height=450)
-
-print(f'Win:{win} Loss:{loss}')
