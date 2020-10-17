d520a02c9679785344f0b3c1c39d9d365198727d
ryan susman <ryansusman@gmail.com>
2020-10-17 10:02:30 -0500

    bot template cleanup

--- connectx_rl/bots/bot_v1/package/submission_template.py
+++ connectx_rl/bots/bot_v1/package/submission_template.py
@@ -1,10 +1,10 @@
+master_truth_table = {REPLACEME}
 def my_agent(observation, configuration):
     from random import choice
     import numpy as np
     this_coice = 0
     _board_width = 7
     _board_height = 6
-    master_truth_table = {REPLACEME}
     obs = np.reshape(observation.board, (_board_width, _board_height)).T
     if str(obs) in master_truth_table:
         this_choice = master_truth_table[str(obs)]
