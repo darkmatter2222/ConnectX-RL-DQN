6cab1ba6cf121fc873b21f0146ce12d6900e2f67
ryan susman <ryansusman@gmail.com>
2020-10-17 08:29:45 -0500

    submission tester and builder

--- connectx_rl/bots/bot_v1/package/submission_template.py
+++ connectx_rl/bots/bot_v1/package/submission_template.py
@@ -4,16 +4,12 @@ def my_agent(observation, configuration):
     this_coice = 0
     _board_width = 7
     _board_height = 6
-
     master_truth_table = {REPLACEME}
-
-    
     obs = np.reshape(observation.board, (_board_width, _board_height)).T
-    
     if str(obs) in master_truth_table:
         this_choice = master_truth_table[str(obs)]
-        #print('chosen')
+        print('chosen')
     else:
         this_choice = choice([c for c in range(_board_width) if observation.board[c] == 0])
-        #print('random')
+        print('random')
     return this_choice
